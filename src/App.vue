fea2a2148c5a4653d3d882b61396f9a3add9a3b0
yuehhan <yuehwhan@gmail.com>
2021-03-11 01:38:20 -0500

    added vuetify and router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/fire.gif">
-    <HelloWorld msg="Let's GEDDIT"/>
-  </div>
+<v-app>
+  <v-navigation-drawer app>
+    <!-- -->
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <!-- -->
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>  |
+    <router-link to="/helloworld">HelloWorld</router-link>
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+
+      <router-view/>
+
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +37,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/fire.gif">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
